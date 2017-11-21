cc2c2f133ad1bf23ab3bf0f224c107051f97d1dd
Fabio Jacob <fabio.oj@gmail.com>
2017-11-21 20:18:27 -0200

    Exercicios do 057 ao 065

---
+++ CursoemVideo/Mundo 2/ex062.py
@@ -0,0 +1,3 @@
+""" ex 062: Melhore o DESAFIO 062, perguntando para o usuário se ele quer
+mostrar mais alguns termos. O programa encerra quando ele disser que quer
+mostrar 0 termos."""
---
+++ CursoemVideo/Mundo 2/ex063.py
@@ -0,0 +1,4 @@
+""" ex 063: Escreva um programa que leia um número n inteiro qualquer e mostre
+na tela os n primeiros elementos de uma sequencia de Fibonacci.
+
+Ex.: 0 - 1 - 1 - 2 - 3 - 5 - 8"""
